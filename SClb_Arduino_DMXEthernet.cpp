2287b05be33c580bb910866a9b89cca2510054e3
Eugene Mikhaylov <statuscue@hes.od.ua>
2019-03-08 19:00:11 +0200

    Add project files.

---
+++ SClb_Arduino_DMXEthernet.cpp
@@ -0,0 +1,10 @@
+/*
+ Name:		SClb_Arduino_DMXEthernet.cpp
+ Created:	08-Mar-19 19:00:06
+ Author:	statu
+ Editor:	http://www.visualmicro.com
+*/
+
+#include "SClb_Arduino_DMXEthernet.h"
+
+
---
+++ SClb_Arduino_DMXEthernet.h
@@ -0,0 +1,19 @@
+/*
+ Name:		SClb_Arduino_DMXEthernet.h
+ Created:	08-Mar-19 19:00:06
+ Author:	statu
+ Editor:	http://www.visualmicro.com
+*/
+
+#ifndef _SClb_Arduino_DMXEthernet_h
+#define _SClb_Arduino_DMXEthernet_h
+
+#if defined(ARDUINO) && ARDUINO >= 100
+	#include "arduino.h"
+#else
+	#include "WProgram.h"
+#endif
+
+
+#endif
+
